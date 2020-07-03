64282ad6ba9fd446f4861c8dc9d6295ffb391503
Timo Scheuermann <timo.scheuermann@gmx.de>
2020-07-04 01:19:39 +0200

    updated icons.json's path

--- src/loadIcons.ts
+++ src/loadIcons.ts
@@ -3,11 +3,7 @@ import { Icon } from './models/Icon.model';
 
 export async function loadIcons(): Promise<void> {
   const xhr = new XMLHttpRequest(); // a new request
-  xhr.open(
-    'GET',
-    'https://raw.githubusercontent.com/TimoScheuermann/Timos-Icons/2.15.2/icons.json',
-    true
-  );
+  xhr.open('GET', 'https://icons.timos.design/resources/icons.json', true);
   xhr.send(null);
   xhr.onload = () => {
     if (xhr.readyState === 4 && xhr.status === 200) {
